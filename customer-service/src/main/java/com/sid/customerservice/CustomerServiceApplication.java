de66936481287e36b0102f235543d2333f0ed84d
nrkrcs <61358048+nrkrcs@users.noreply.github.com>
2020-12-31 12:11:22 +0100

    Add files via upload

---
+++ customer-service/src/main/java/com/sid/customerservice/CustomerServiceApplication.java
@@ -0,0 +1,28 @@
+package com.sid.customerservice;
+
+import com.sid.customerservice.entities.Customer;
+import com.sid.customerservice.repository.CustomerRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class CustomerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerServiceApplication.class, args);
+    }
+    @Bean
+    CommandLineRunner start(CustomerRepository customerRepository){
+        return args -> {
+            customerRepository.save(new Customer(null, "Yassine", "yassine@gmail.com"));
+            customerRepository.save(new Customer(null, "Adnane", "adnane@gmail.com"));
+            customerRepository.save(new Customer(null, "FZ", "fz@gmail.com"));
+            customerRepository.findAll().forEach(c -> {
+                System.out.println(c.toString());
+            });
+        };
+    }
+
+}
---
+++ customer-service/src/main/java/com/sid/customerservice/entities/Customer.java
@@ -0,0 +1,21 @@
+package com.sid.customerservice.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.springframework.data.rest.core.config.Projection;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@AllArgsConstructor @NoArgsConstructor @Data @ToString
+public class Customer {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+}
